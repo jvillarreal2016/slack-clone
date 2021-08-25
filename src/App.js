2a67ef4b12cc335e1302da8e29cd12ae552294de
Javi Villarreal <javivillarreal@javis-mbp.lan>
2021-08-25 02:19:06 -0500

    slack clone

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
+import React, { useState } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Switch, Route }
+from 'react-router-dom';
+import Chat from './Chat';
+import Login from './Login';
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="App">
-        <h2>hello world</h2>
+        <Router>
+          {!user ? (
+            <Login />
+          ) : (
+          <>
+          <Header />
+            <div className="app__body">
+              <Sidebar />
 
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          {/* React-Router -> Chat screen */}
-        </div>
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+
+            {/* React-Router -> Chat screen */}
+            </div>
+            </>
+            )}
+        </Router>
     </div>
   );
 }
